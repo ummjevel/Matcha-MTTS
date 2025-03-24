ebabade6a6a9951d1c2252dfe5a94f19b779d10c
ummjevel <mseagle2023@gmail.com>
2025-03-24 12:53:09 +0000

    update cleaner using specific cleaner from cleaners

--- matcha/text/__init__.py
+++ matcha/text/__init__.py
@@ -6,6 +6,10 @@ from matcha.text.symbols import symbols
 _symbol_to_id = {s: i for i, s in enumerate(symbols)}
 _id_to_symbol = {i: s for i, s in enumerate(symbols)}  # pylint: disable=unnecessary-comprehension
 
+from matcha.text.symbols import language_id_map
+_lang_to_id = {s: i for i, s in enumerate(language_id_map)} # {"EN": 0, 'KR': 1, "ZH": 2, "JP": 3}
+_id_to_lang = {i: s for i, s in enumerate(language_id_map)} # {0: "EN", 1: 'KR', 2: "ZH", 3: "JP"}
+
 
 class UnknownCleanerException(Exception):
     pass
@@ -23,6 +27,12 @@ def text_to_sequence(text, cleaner_names, language_code=None, diff_gtp=False):
 
     if language_code and not diff_gtp:
         clean_text = _clean_text_ml(text, cleaner_names[0], language_code)
+        for symbol in clean_text:
+            symbol_id = _symbol_to_id[symbol]
+            sequence += [symbol_id]
+        return sequence, clean_text
+    elif language_code and diff_gtp:
+        clean_text = _clean_text_mls(text, cleaner_names, language_code)
         for symbol in clean_text:
             symbol_id = _symbol_to_id[symbol]
             sequence += [symbol_id]
@@ -60,8 +70,18 @@ def _clean_text(text, cleaner_names):
         cleaner = getattr(cleaners, name)
         if not cleaner:
             raise UnknownCleanerException(f"Unknown cleaner: {name}")
-        text = cleaner(text)
-    return text
+        text_cleaned = cleaner(text)
+    return text_cleaned
+
+
+def _clean_text_mls(text, cleaner_names, lang):
+    # for name in cleaner_names:
+    name = cleaner_names[_lang_to_id[lang]]
+    cleaner = getattr(cleaners, name)
+    text_cleaned = cleaner(text)
+    # print(f"lang : {lang}, text : {text} -> {text_cleaned}")
+    # exit(1)
+    return text_cleaned
 
 
 def _clean_text_ml(text, cleaner, language_code):
